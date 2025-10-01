36f36244ad5c936ca9ff21ab305a4e1e6237b154
Capela <andrew@Capelagames.com.au>
2025-10-01 11:53:07 +1000

    doubley linked list

--- Assets/Scripts/MyLinkedList.cs
+++ Assets/Scripts/MyLinkedList.cs
@@ -9,6 +9,7 @@ namespace OurLinkedList
         public int age = 0;
 
         public Node next;
+        public Node prev;
 
         public Node(string name, int age)
         {
@@ -16,6 +17,7 @@ namespace OurLinkedList
             this.age = age;
 
             next = null;
+            prev = null;
         }
     }
     
@@ -45,8 +47,14 @@ namespace OurLinkedList
             linkedList.Next();
             linkedList.Next();
             linkedList.DeleteNext();
-
+            
             linkedList.PrintAll();
+            
+            linkedList.ReturnToHeader();
+            linkedList.Next();
+            linkedList.Next();
+            linkedList.Prev();
+            linkedList.PrintCurrent();
         }
     }
 
@@ -54,12 +62,15 @@ namespace OurLinkedList
     {
         private Node current;
         private Node header;
+        private Node tail;
 
         public LinkedList(Node node)
         {
             header = node;
+            tail = node;
             current = node;
             header.next = null;
+            header.prev = null;
         }
 
         public void InsertNext(Node newNode)
@@ -68,13 +79,37 @@ namespace OurLinkedList
             {
                 current.next = newNode;
                 newNode.next = null;
+                newNode.prev = current;
+                tail = newNode;
             }
             else
             {
                 newNode.next = current.next;
                 current.next = newNode;
+
+                newNode.next.prev = newNode;
+                newNode.prev = current;
             }
             current = newNode; // Optional
+        }
+
+        public void InsertPrev(Node newNode)
+        {
+            if (current.prev == null)
+            {
+                header = newNode;
+                current.prev = newNode;
+                newNode.next = current;
+                newNode.prev = null;
+            }
+            else
+            {
+                newNode.prev = current.prev;
+                current.prev = newNode;
+                newNode.prev.next = newNode;
+                newNode.next = current;
+            }
+            current = newNode;
         }
 
         public bool Next()
@@ -87,19 +122,81 @@ namespace OurLinkedList
 
             return false;
         }
+        public bool Prev()
+        {
+            if (current.prev != null)
+            {
+                current = current.prev;
+                return true;
+            }
 
+            return false;
+        }
+        
+        
         public bool DeleteNext()
         {
             if (current.next == null) return false;
 
             current.next = current.next.next;
 
+            if (current.next == null)
+            {
+                tail = current;
+            }
+            else
+            {
+                current.next.prev = current;
+            }
+            // if you're using c++ raw pointers
+            // you would have delete() here
             return true;
+        }
+        
+        public bool DeletePrev()
+        {
+            if (current.prev == null) return false;
+
+            current.prev = current.prev.prev;
+
+            if (current.prev == null)
+            {
+                header = current;
+            }
+            else
+            {
+                current.prev.next = current;
+            }
+            // if you're using c++ raw pointers
+            // you would have delete() here
+            return true;
+        }
+
+        public void DeleteCurr()
+        {
+            if (Prev())
+            {
+                DeleteNext();
+            }
+            else if(Next())
+            {
+                DeletePrev();
+            }
+            else
+            {
+                header = null;
+                tail = null;
+                current = null;
+            }
         }
 
         public void ReturnToHeader()
         {
             current = header;
+        }
+        public void ReturnToTail()
+        {
+            current = tail;
         }
 
         public void PrintCurrent() => Debug.Log(current.name + " aged " + current.age);
@@ -114,6 +211,17 @@ namespace OurLinkedList
             {
                 Print(currentPrint);
                 currentPrint = currentPrint.next;
+            } while (currentPrint != null);
+        }
+        
+        public void PrintAllReverse()
+        {
+            if (tail == null) return;
+            Node currentPrint = tail;
+            do
+            {
+                Print(currentPrint);
+                currentPrint = currentPrint.prev;
             } while (currentPrint != null);
         }
     }
